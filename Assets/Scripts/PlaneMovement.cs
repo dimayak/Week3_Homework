ed3cc39218cdd5b5d4a5108cc1481068fbbb0412
dimayak <412813+dimayak@users.noreply.github.com>
2024-02-20 23:33:56 +0100

    Basic movement

--- Assets/Scripts/PlaneMovement.cs
+++ Assets/Scripts/PlaneMovement.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlaneMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float MovementSpeed = 1.0f;
+    public float RotationSpeed = 1.0f;
+
+    private Rigidbody _rb = default;
+    float _movement = 0.0f;
+    float _rotation = 0.0f;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        HandleInput();
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.AddRelativeForce(_movement * Vector3.right * MovementSpeed, ForceMode.VelocityChange);
+        _rb.AddRelativeTorque(0.0f, 0.0f, _rotation * RotationSpeed, ForceMode.VelocityChange);
+    }
+
+    private void HandleInput()
+    {
+        _movement = Input.GetAxis("Horizontal");
+        _rotation = Input.GetAxis("Vertical");
     }
 }
