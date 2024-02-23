521b27b4ba22d33c9a47e4c74f403b6f069b948c
dimayak <412813+dimayak@users.noreply.github.com>
2024-02-23 23:59:23 +0100

    Gas can

--- Assets/Scripts/Spawn/EntityFactory.cs
+++ Assets/Scripts/Spawn/EntityFactory.cs
@@ -12,7 +12,7 @@ public class EntityFactory<T>: IEntityFactory<T> where T: Entity
     public T Create(Transform spawnPoint)
     {
         EntityData entityData = _data[Random.Range(0, _data.Length)];
-        GameObject instance = GameObject.Instantiate(entityData.prefab, spawnPoint.position, spawnPoint.rotation);
+        GameObject instance = GameObject.Instantiate(entityData.prefab, spawnPoint.position, spawnPoint.rotation, spawnPoint.parent);
         return instance.GetComponent<T>();
     }
 }
